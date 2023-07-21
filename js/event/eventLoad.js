ee8e8059ffb3af3a6b73464b610a309219f59fc9
justin-jig <justin-j@naver.com>
2023-07-21 20:45:09 +0900

    20230721.20 eventpage

--- js/event/eventLoad.js
+++ js/event/eventLoad.js
@@ -1,29 +1,35 @@
 
-let datalist = [];
-var dataFilterlist = [];
-var dataFilterlistPirce = [];
-let hashString = window.location.hash.replace('#','');  
+let datalist = []; // 행사 list 
+var dataFilterlist = []; // 행사 분류 filter list
+var dataFilterlistPirce = []; // 행사 가격 filter list
+let hashString = window.location.hash.replace('#',''); // url window.location.hash 값  
 
-$(window).ready( async function(){ 
-    datalist = await getEventDat();
-    if (window.location.search === "?detail") { // detail 페이지
+$(window).ready(async function(){ 
+    
+    datalist = await getEventDat(); // 행사 리스트 data fetch
+
+    if (window.location.search === "?detail") { // detail 페이지 일 때
                 
         if ($('#eventItemWrap')) {
-             // 스크롤 이벤트 삭제 및 css 
+
+             // 스크롤 이벤트, css 삭제 및 clasename 추가
             window.removeEventListener('scroll', eventScroll);
             $('#eventItemWrap').css('display','none');
 
             if ($('#eventListFillter')) {
                 $('#eventListFillter').remove();
             }
+
             if ($('#eventContainer > h2')) {
                 $('#eventContainer > h2').remove();
             }
+
+            $('.event-banner').addClass('detail-banner');
         }
 
         $('#eventDetailWrap').css('display','block');
 
-        eventDetailDatavind(hashString, datalist);
+        eventDetailDatavind(hashString, datalist); // detail html 생성 및 data 바인딩
 
     } else { // 리스트 페이지
 
@@ -45,6 +51,7 @@ $(window).ready( async function(){
         if ($('#eventDetailWrap')) {
             $('#eventDetailWrap').css('display','none')
         }
+        $('.event-banner').removeClass('detail-banner');
        
         // 스크롤 이벤트 생성 
         window.addEventListener('scroll', eventScroll);
@@ -52,13 +59,20 @@ $(window).ready( async function(){
     }
 }); 
 
-const eventScroll = () => {
+const eventScroll = () => { // 리스트 무한스크롤
 
     if (window.innerHeight + window.scrollY + window.innerHeight/9 >= document.body.scrollHeight) {
 
-        if (dataFilterlist.length === 0) {
-            eventItemAdd(datalist);
-        } else {
+        if (dataFilterlist.length === 0) { // 분류를 선택 x
+          
+            if (dataFilterlistPirce.length > 0) {
+
+                eventItemAdd(dataFilterlistPirce);
+            } else {
+                eventItemAdd(datalist);
+            }
+        
+        } else { // 분류를 선택 o
 
             if (dataFilterlistPirce.length === 0) {
 
@@ -69,13 +83,4 @@ const eventScroll = () => {
          
         }
     }
-}
-
-
-const eventListSelctChange = (e) => {
-
-    console.log(e.value)
-    console.log(datalist.filter((value) => value.CODENAME === e.value ));
-    window.location.href = '../event.html#' +JSON.stringify(e.value);
-
 }
